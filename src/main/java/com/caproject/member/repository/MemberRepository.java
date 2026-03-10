caf4e2110d929fdda3b28b5da4249032f1d4ef53
hyun2ee2 <Hongkobi04@gmail.com>
2026-03-10 22:43:41 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/com/caproject/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.caproject.member.repository;
+
+import com.caproject.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
